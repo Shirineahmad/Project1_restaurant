c42f545e46f635e57b2e79cd301aaef2acbd4d87
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-09 15:35:09 +0300

    update to responsive

---
+++ home.js
@@ -0,0 +1,6 @@
+
+let dropdownButton = document.getElementById("burger-menu");
+    let dropdown = document.getElementById("main-div");
+    dropdownButton.addEventListener("click", function () {
+        dropdown.classList.toggle("hide-element")
+    });
