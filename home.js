65b04b0ae840d1ce2b58160981a4bbe36f6bfd5a
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-10 10:41:01 +0300

    edit responsive

--- home.js
+++ home.js
@@ -1,6 +1,13 @@
 
-let dropdownButton = document.getElementById("burger-menu");
-    let dropdown = document.getElementById("main-div");
-    dropdownButton.addEventListener("click", function () {
-        dropdown.classList.toggle("hide-element")
-    });
+const burgerMenu = document.querySelector(".burger-menu");
+const navLinks = document.querySelector(".nav-links");
+console.log(burgerMenu);
+console.log(navLinks);
+
+
+    
+burgerMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('burger-menu')) {
+        navLinks.classList.toggle('active');
+    }
+})
